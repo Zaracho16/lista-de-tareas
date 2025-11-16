d4cc24f034ad433f4c7a5ef38844b125d81c8269
Adrian Zaracho <adrianzaracho16@gmail.com>
2025-11-16 18:04:58 -0300

    Al hacer check la fila se tacha y la opacidad baja

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ botonAgregar.addEventListener('click', () => {
         <input type="checkbox" class="check-lista" id="idCheck">
         <div>
             <img width="24" height="24" src="https://img.icons8.com/material/24/FA5252/trash--v1.png" class="icono-basurero" alt="trash--v1"/>
-        </div> 
+        </div>
     `;
 
     contadorFila++;
@@ -26,12 +26,13 @@ botonAgregar.addEventListener('click', () => {
 
     // funcionalidades al marcar el checkbox en la lista
     const checkbox = nuevaFila.querySelector('.check-lista');
-
     const mensajeeCheck = document.getElementById('mensajeCheck');
+
+    const fila = nuevaFila;
 
     let timeOutMensaje;
     checkbox.addEventListener('change', () => {
-
+    
     clearTimeout(timeOutMensaje);
 
     if(checkbox.checked) {
@@ -40,8 +41,13 @@ botonAgregar.addEventListener('click', () => {
         timeOutMensaje = setTimeout(() => {
             mensajeeCheck.style.display = 'none';
         }, 1000);
+
+        // cuando se complete una tarea dejar el texto tachado
+        fila.classList.add('contenedor-filas-t');
+
     } else {
         mensajeeCheck.style.display = 'none';
+        fila.classList.remove('contenedor-filas-t');
     }
 
 });
